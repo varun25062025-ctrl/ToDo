896dcfc146d2152e9f490e0129c6eb9ea509e377
varun25062025-ctrl <varun25062025@gmail.com>
2026-05-04 17:34:26 +0530

    feat(react): add reorder/move actions

--- examples/react/src/todo/constants.js
+++ examples/react/src/todo/constants.js
@@ -1,7 +1,1 @@
-export const ADD_ITEM = "ADD_ITEM";
-export const UPDATE_ITEM = "UPDATE_ITEM";
-export const REMOVE_ITEM = "REMOVE_ITEM";
-export const TOGGLE_ITEM = "TOGGLE_ITEM";
-export const REMOVE_ALL_ITEMS = "REMOVE_ALL_ITEMS";
-export const TOGGLE_ALL = "TOGGLE_ALL";
-export const REMOVE_COMPLETED_ITEMS = "REMOVE_COMPLETED_ITEMS";
+export const ADD_ITEM = \"ADD_ITEM\";\nexport const UPDATE_ITEM = \"UPDATE_ITEM\";\nexport const REMOVE_ITEM = \"REMOVE_ITEM\";\nexport const TOGGLE_ITEM = \"TOGGLE_ITEM\";\nexport const REMOVE_ALL_ITEMS = \"REMOVE_ALL_ITEMS\";\nexport const TOGGLE_ALL = \"TOGGLE_ALL\";\nexport const REMOVE_COMPLETED_ITEMS = \"REMOVE_COMPLETED_ITEMS\";\n\n// Drag & drop reordering\nexport const REORDER_ITEMS = \"REORDER_ITEMS\";\n// Keyboard fallback controls\nexport const MOVE_ITEM_UP= \"MOVE_ITEM_UP\";\nexport const MOVE_ITEM_DOWN = \"MOVE_ITEM_DOWN\";\n
